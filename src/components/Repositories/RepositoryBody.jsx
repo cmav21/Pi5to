12b51b4ff6c4e1873d751a465ceb58a085c407cd
Gerald Amezcua <gamezcua1@ucol.mx>
2018-11-07 07:53:32 -0600

    FIX responsiveness and height of image

--- src/components/Repositories/RepositoryBody.jsx
+++ src/components/Repositories/RepositoryBody.jsx
@@ -4,13 +4,17 @@ import styled from 'styled-components'
 import imgPicker from '../../assets/modalImagePicker'
 
 const Parallax = styled.div`
-  background: url(${props => props.img});
+  background: url(${props => props.img}) repeat-y center top;
   filter: brightness(30%);
   background-attachment: fixed;
   background-size: 100%;
   height: 50vh;
   color: white;
-`
+
+  @media (max-width: 500px) {
+    height: 70vh;
+  }
+`;
 
 class RepositoryBody extends Component {
   style = {
@@ -27,11 +31,11 @@ class RepositoryBody extends Component {
       return <h1>ha ocurrido un error</h1>
 
     return <div className=" hero-body columns is-multiline" style={this.style.heroStyle}>
-        <div className="column is-12">
+        <div className="column is-12-mobile is-12-desktop is-12-tablet">
           <Parallax img={imgPicker()}></Parallax>
           <div className="content" style={this.style.box}>
             <h1 className="title is-2" style={this.style.titles}>{repo.nombre}</h1>
-            <p className="subtitle is-6" style={this.style.titles}>{String(repo.etiquetas).split(',').join(' #')}</p>
+            <p className="subtitle is-6" style={this.style.titles}>{String("#"+repo.etiquetas).split(',').join(' #')}</p>
             <div className="column is-8">
               <p className="title is-4" style={this.style.titles}>Descripción </p>
               <p style={{ marginLeft: "1vw" }} style={this.style.titles}>{repo.descripcion}</p>
