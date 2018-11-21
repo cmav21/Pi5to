f4597b7690e2a3df189da4ce7ae5e3d8086081f5
cesar magallon <cesar9mv@gmail.com>
2018-11-20 19:59:40 -0600

    [fix] notifications

--- src/components/Resources/AddResource.jsx
+++ src/components/Resources/AddResource.jsx
@@ -3,50 +3,45 @@ import TopBar from '../HomePage/TopBar';
 import Form from './ResourceForm';
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
+import { resourceAdded} from '../../actions/UserActions'
 
 class AddResource extends Component {
     state = {
         added: "",
-        addClass: "notification is-danger"
+        cleanNotification: ""
     }
 
     componentWillReceiveProps(nextProps){
         this.setState({
-            added: nextProps.users.resourceAdded.added
+            added: nextProps.users.resourceAdded.added,
+            cleanNotification: nextProps.users.notification
         })
     }
 
     dismissNotification = () => {
-        if(this.state.addClass == "notification is-danger") {
-            this.setState({
-                added: "hola",
-                addClass: "notification is-danger is-hidden"
-            });
-        }
+        this.props.resourceAdded({'added':""})
     }
     
     renderNotification = () => {
-        if(this.state.added === "true") {
+        if(this.state.added == "false" ) {
+            return(
+                <div className="notification is-danger">
+                    <button className="delete" onClick = {this.dismissNotification}></button>
+                    Ups!, hay un problema con los datos introducidos
+                </div>
+            );
+        }
+        else if(this.state.added == "true" ){
             return(
                 <div className="notification is-success">
-                    <button className="delete"></button>
+                    <button className="delete" onClick = {this.dismissNotification}></button>
                     El recurso ha sido agregado con exito
                 </div>
             );
         }
-        else if(this.state.added === "false") {
-            if(this.state.addClass == "notification is-danger is-hidden") {
-                this.setState({
-                    addClass: "notification is-danger"
-                });
-            }
-            return(<div className={this.state.addClass}>
-                <button className="delete" onClick = {this.dismissNotification}></button>
-                Ups!, hay un problema con los datos introducidos
-            </div>)
-        }
     }
     render() {
+        console.log(this.state);
         return(
             <div className="hero is-light is-fullheight">
                 <TopBar/>
@@ -56,13 +51,10 @@ class AddResource extends Component {
                 }
             </div>
         );
+        
     }
 }
 
-let resourceRegister = connect( state => ({
-  users: state.users
-}),{
-
-})(AddResource);
+let resourceRegister = connect( state => ({users: state.users}),{resourceAdded})(AddResource);
 
 export default withRouter(resourceRegister);
